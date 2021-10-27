f79705761d2e1817fd6964d9b99b7d625cf2e454
Daiene Fortunato <dayne.cl@gmail.com>
2021-10-27 19:12:22 +0000

    separei a url e limpei a sintaxe

--- api/pypi.py
+++ api/pypi.py
@@ -1,17 +1,25 @@
 import requests
 
 def version_exists(package_name, version):
-    response = requests.get(f"https://pypi.org/pypi/{package_name}/{version}/json")
+    
+    repository = f"https://pypi.org/pypi/{package_name}/{version}/json"
+    response = requests.get(repository)
+    
     if response.status_code == 200:
         return True
+    
     else:
         return False
 
 
 def latest_version(package_name):
-    response = requests.get(f"https://pypi.org/pypi/{package_name}/json")
+    
+    repository = f"https://pypi.org/pypi/{package_name}/json"
+    response = requests.get(repository)
+    
     if response.status_code == 404:
-        return "None"
+        return None
+    
     else:
         response = response.json()
         return list(response['releases'].keys())[-1]
